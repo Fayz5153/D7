dd180c986d9df3c7e0edc6c293a2e5aeaa83bbe1
F4Teacher <for.git.5153@gamil.com>
2023-05-27 16:44:41 +0500

    mode

--- src/views/Navbar.jsx
+++ src/views/Navbar.jsx
@@ -1,5 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import { Link } from 'react-router-dom';
+import moon from "./moon.svg"
+import sun from "./sun.svg"
 
 const Navbar = () => {
     const [scroll, setScroll] = useState(0);
@@ -11,13 +13,19 @@ const Navbar = () => {
         window.addEventListener("scroll", onScroll)
         return () => window.removeEventListener("scroll", onScroll)
     },[])
-
+    const [mode, setMode] = useState(true);
+    const handleMode = () =>{
+        setMode(!mode)
+    }
     // console.log(scroll)
     return ( 
         <div className={scroll > 100 ? 'navbar scrolled' : 'navbar'}>
             <Link to={"/"}>Home</Link>
             <Link to={"/about"}>About</Link>
             <Link to={"/category"}>Category</Link>
+            <button onClick={handleMode} className={mode ? "moon btn" : "sun btn"}>
+                <img src={mode ? moon : sun} alt="" width={25}/>
+            </button>
         </div>
      );
 }
